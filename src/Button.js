55cb810e811cfc6fb13dacafb108d16f5120214a
gathoni-k <maryg.kahiga@gmail.com>
2020-08-22 11:27:36 +0300

    feat: Add dark mode functionality
    Get mode from ThemeContext
    Add an onClick handler to toggleTheme and add dark-mode class to html element

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import "./Button.css"
+import ThemeContext from './context/ThemeContext'
 import sun from './sun.svg'
+import moon from './moon.svg'
 
 export default () => {
+  const {mode, toggleMode} = useContext(ThemeContext)
+  const handleClick = () => {
+    toggleMode()
+    let root = document.documentElement;
+    if (mode === "light") {
+      root.classList.add("dark-mode")
+    } else {
+      root.classList.remove("dark-mode")
+    }
+  }
   return (
     <div className="toggle-btn">
-    <button className="toggle-btn"><img src={sun} alt="mode" className="mode-img"/></button>
+    <button onClick={handleClick} style={{
+      backgroundColor:mode === "light"?'transparent':'#eeeeee'
+    }} className="toggle-btn"><img src={mode === "light"?sun:moon} alt="logo" className="mode-img"/></button>
     </div>
   )
 }
