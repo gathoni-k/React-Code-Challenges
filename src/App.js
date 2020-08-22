d5d8295d3f9183e2fa4f4db85c9bb6cb60923f9b
gathoni-k <maryg.kahiga@gmail.com>
2020-08-22 11:23:50 +0300

    chore: Use ThemeContext/wrap .App with ThemeContext Provider
    Propagate context value to Button child componentby wrapping parem=nt with the Provider.
    Pass in the default value and update function for setting the mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Theme.css'
 import Button from './Button';
+import ThemeContext from './context/ThemeContext';
 
 function App() {
+  const [mode, setmode] = useState("light")
+  const toggleMode = () => {
+    setmode((mode) => {
+      mode === "light"?setmode("dark"):setmode("light")
+  })
+}
+
   return (
+    <ThemeContext.Provider
+      value={{
+        mode,
+        toggleMode
+      }}
+    >
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,6 +37,7 @@ function App() {
       </header>
       <Button/>
     </div>
+    </ThemeContext.Provider>
   );
 }
 
